b57112265f491357eeed18c289c6c9f4aae67ae7
Victoria Goulko <vgoulko@seek.com.au>
2019-12-02 13:50:41 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {CardList} from './components/card-list/card-list.components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchFields: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        // .then(response => console.log(response))
+        .then(response =>response.json())
+        .then(users => this.setState({monsters:users}))
+  }
+
+  render() {
+      const{monsters, searchFields} = this.state;
+      const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchFields.toLowerCase()));
+    return (
+        <div className="App">
+          <input
+              type='search'
+              placeholder='search monster'
+              // onChange={e => console.log(e.target.value)}
+              // onChange={e => this.setState({searchFields: e.target.value}, () => console.log(this.state))}
+              onChange={e => this.setState({searchFields: e.target.value})}
+
+          />
+          <CardList monsters={filteredMonsters} />
+        </div>
+    )
+  }
 }
 
 export default App;
