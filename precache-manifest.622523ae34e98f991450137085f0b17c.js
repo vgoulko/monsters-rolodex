b0b65e231673e801c7d4aedd650a160f0c1407dd
Victoria Goulko <vgoulko@seek.com.au>
2020-01-30 10:48:43 +1100

    Updates

---
+++ precache-manifest.622523ae34e98f991450137085f0b17c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96719513fc1a02bf14cd1e19a708ba38",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3f3ba1163c02d9719195",
+    "url": "/monsters-rolodex/static/css/main.2d5b9af8.chunk.css"
+  },
+  {
+    "revision": "3a057e02db196afa0ca3",
+    "url": "/monsters-rolodex/static/js/2.f833121f.chunk.js"
+  },
+  {
+    "revision": "3f3ba1163c02d9719195",
+    "url": "/monsters-rolodex/static/js/main.e3279a40.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
